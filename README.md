cc966ba8da27d9c4a52cab867a7e903673c50b85
GhostMac <theghostmac@gmail.com>
2021-09-22 21:29:02 +0100

    portfolio site built with serveMux multiplexor

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a simple personal portfolio site built with Golang as the backend.
+The HTTP router used here is the standard ServeMux multiplexor.
+Other portfolio projects in this directory will be built with 
+other routers like Gorilla Mux.
--- main.go
+++ main.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"net/http"
-	"log"
-	"html/template"
-	
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate, _ := template.ParseFiles("templates/home.html")
-	homeTemplate.Execute(w, nil)
-}
-
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", home).Methods("GET")
-	fileServer := http.FileServer(http.Dir("static"))
-	router.PathPrefix("/").Handler(http.StripPrefix("/static/", fileServer))
-	log.Fatalln(http.ListenAndServe(":3000", nil))
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/", fs)
+	http.ListenAndServe(":8080", mux)
 }
